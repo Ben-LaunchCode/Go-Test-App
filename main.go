88139987d730da259d605039a85cfd6c4c424e62
Benjamin W. Shutt <ben@launchcode.org>
2018-01-16 16:49:17 -0800

    Web Hello World

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func determineListAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr, err := determineListAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", hello)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
